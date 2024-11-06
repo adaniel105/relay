9b5e12f73c0379255215f6d19bc4f235a31d21da
Akinola Daniel <vantablack294@gmail.com>
2024-11-06 23:40:15 +0100

    Decouple utilities from main

--- yt.py
+++ yt.py
@@ -1,5 +1,4 @@
-import requests
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -7,85 +6,16 @@ from langchain.schema.runnable.passthrough import RunnableAssign
 from langchain.schema.runnable import RunnableLambda
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from operator import itemgetter
+from api import yt
+from models.know_base import KnowledgeBase
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-yt_api_key = os.getenv("YT_API_KEY")
 nvidia_api_key = os.getenv("NVIDIA_API_KEY")
 
-
-url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&forHandle=sarah_frags&key={yt_api_key}"
 chat_llm = ChatNVIDIA(model="meta/llama-3.2-3b-instruct") | StrOutputParser()
 instruct_llm = ChatNVIDIA(model="meta/llama3-70b-instruct") | StrOutputParser()
-
-res = requests.get(url)
-res = res.json()
-
-name = res["items"][0]["snippet"]["title"]
-description = res["items"][0]["snippet"]["description"]
-# creation_date = res["items"][0]["snippet"]["publishedAt"] use after formatting
-total_views = res["items"][0]["statistics"]["viewCount"]
-total_subs = res["items"][0]["statistics"]["subscriberCount"]
-video_count = res["items"][0]["statistics"]["videoCount"]
-
-# splitting this later so i read into dict instead of directly (also for readability)
-# real life this will probably run async and store responses in cache for search, but for now, let's index directly.
-
-response = {
-    "dict1": {
-        "name": name.lower(),
-        "description": description.lower(),
-        "total_views": total_views,
-        "total_subs": total_subs,
-        "video_count": video_count,
-    }
-}
-
-
-def get_yt_info(d: dict) -> str:
-    req_keys = ["name"]
-    assert all(
-        (key in d) for key in req_keys
-    ), f"Expected dictionary with keys {req_keys}, got {d}"
-
-    ## Static dataset. get_key and get_val can be used to work with it, and db is your variable
-    keys = req_keys + ["description", "total_views", "total_subs", "video_count"]
-    idx = [
-        response["dict1"]["name"],
-    ]
-    # ideally this is a for loop looping through all variables in the dict of dicts
-    if idx[0] == response["dict1"].get("name"):
-        values = [
-            [
-                response["dict1"]["name"],
-                response["dict1"]["description"],
-                response["dict1"]["total_views"],
-                response["dict1"]["total_subs"],
-                response["dict1"]["video_count"],
-            ]
-        ]
-    else:
-        print("Channel not found")
-
-    # used to fetch required keys.
-    get_key = lambda d: "|".join(
-        [d["name"]]
-    )  # doesn't work because we can't match strings to their ints
-    get_val = lambda l: {k: v for k, v in zip(keys, l)}
-    db = {get_key(get_val(entry)): get_val(entry) for entry in values}
-
-    # Search for the matching entry
-    data = db.get(get_key(d))
-    if not data:
-        return (
-            f"Based on {req_keys} = {get_key(d)}) from your knowledge base, no info on the user's channel was found."  # split and replace with only required keys
-            " This process happens every time new info is learned. If it's important, ask them to confirm this info."
-        )
-    return f"{data['name']}'s channel contains {data['video_count']} videos, a total of {data['total_views']} views on all videos, and a community of {data['total_subs']} subscribers"
-
-
-# print(get_yt_info({"name": "sarahcat"}))
 
 
 def RExtract(pydantic_class, llm, prompt):
@@ -113,22 +43,6 @@ def RExtract(pydantic_class, llm, prompt):
         return string
 
     return instruct_merge | prompt | llm | preparse | parser
-
-
-class KnowledgeBase(BaseModel):
-    name: str = Field(
-        "general", description="Name or handle of personality of interest"
-    )
-    discussion_summary: str = Field(
-        "",
-        description="Summary of discussion so far, including locations, issues, etc.",
-    )
-    open_problems: str = Field("", description="Topics that have not been resolved yet")
-    current_goals: str = Field("", description="Current goal for the agent to address")
-    response: str = Field(
-        "unknown",
-        description="An ideal response to the user based on their new message",
-    )
 
 
 def get_key_fn(base: BaseModel) -> dict:
@@ -176,7 +90,7 @@ parser_prompt = ChatPromptTemplate.from_template(
 )
 
 knowbase_getter = RExtract(KnowledgeBase, instruct_llm, parser_prompt)
-database_getter = itemgetter("know_base") | get_key | get_yt_info
+database_getter = itemgetter("know_base") | get_key | yt.get_info
 
 
 internal_chain = RunnableAssign({"know_base": knowbase_getter}) | RunnableAssign(
